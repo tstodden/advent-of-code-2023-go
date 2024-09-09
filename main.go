47479e0b12321f278afdea9d6191cbd55a3d7ae8
Tyler Stodden <tylersstodden@gmail.com>
2024-09-08 20:43:11 -0400

    Update main.go to solve day1 part1

--- main.go
+++ main.go
@@ -14,12 +14,19 @@ func main() {
 		log.Fatal(err)
 	}
 
+	total := 0
+	contents := day1.BagContents{Blue: 14, Green: 13, Red: 12}
 	lines := strings.Split(string(file), "\n")
 	for _, v := range lines {
 		game, err := day1.NewGame(v)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("%#v\n", game)
+
+		if game.IsPossible(contents) {
+			total += game.Id
+		}
 	}
+
+	fmt.Printf("total is: %d", total)
 }
